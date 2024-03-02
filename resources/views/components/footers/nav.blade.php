4c697fbdf0319b54ce6cacfc6f82f13effd67056
jp-avr <joaopedroazevedoreisveras@gmail.com>
2024-03-02 19:10:02 -0300

    navbar

---
+++ resources/views/components/footers/nav.blade.php
@@ -0,0 +1,15 @@
+<footer class="footer" style="background-color: #444444">
+    <div class="container-fluid">
+        <div class="row text-muted">
+            <p class="mb-0" style="padding-bottom: 15px;padding-top: 15px;">
+                <a class="text-muted d-flex justify-content-center align-items-center" href="#" target="_blank" style="color: #EBEBEB;">
+                    <strong>
+                        &copy;2021 Simetra - Laboratório Veterinário.
+                        <br>
+                        Todos os direitos reservados.
+                    </strong>
+                </a> 
+            </p>
+        </div>
+    </div>
+</footer>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,6 +77,7 @@
         <main class="py-4">
             @yield('content')
         </main>
+        @include('components.footers.nav')
     </div>
 </body>
 </html>
